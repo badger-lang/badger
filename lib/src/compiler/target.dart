5b3775a7fa65b7dc8c1bb37a2b055f4cb337848c
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-04 22:44:20 -0500

    Tweak to Target

--- lib/src/compiler/target.dart
+++ lib/src/compiler/target.dart
@@ -1,8 +1,8 @@
 part of badger.compiler;
 
 abstract class Target<T> {
-  static Target<String> JS_TARGET = new JsTarget();
-  static Target<String> AST_TARGET = new AstTarget();
+  static final Target<String> JS_TARGET = new JsTarget();
+  static final Target<String> AST_TARGET = new AstTarget();
 
   T compile(Program program);
 }
